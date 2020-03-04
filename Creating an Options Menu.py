606e0b2e8181a5215205daeb3c5bcad727e88df1
Andrezza Nascimento <andrezzacn_1981@hotmail.com>
2020-03-04 10:46:03 -0300

    Creating an Options Menu
    Create a program that reads two values and shows a menu next to the screen. Your program should perform the requested operation in each case.

---
+++ Creating an Options Menu.py
@@ -0,0 +1,11 @@
+valor1 = int(input('Value one: '))
+valor2 = int(input('Value two: '))
+option = 0
+while option !=5:
+    print('''[1]add
+    [2]multiply
+    [3]bigger
+    [4]new numbers
+    [5]exit the program''')
+    option = str(input("What is your option?"))
+print('END OF PROGRAM')
